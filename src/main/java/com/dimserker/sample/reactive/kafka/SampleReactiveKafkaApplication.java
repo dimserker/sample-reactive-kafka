da515f055fe397eb1c00197cc541b518c49ec4dc
dimserker <dimserker@gmail.com>
2020-10-02 12:04:54 +0300

    initial commit

---
+++ src/main/java/com/dimserker/sample/reactive/kafka/SampleReactiveKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.dimserker.sample.reactive.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleReactiveKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleReactiveKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dimserker/sample/reactive/kafka/SampleReactiveKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimserker.sample.reactive.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleReactiveKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
